b1f4381821114441d44ac6dfcdf0c36c21f54803
Durai <durai@Durais-MacBook-Pro.local>
2018-05-10 10:07:53 -0700

    update the new changes

--- src/com/orange/practice/EvenNumber.java
+++ src/com/orange/practice/EvenNumber.java
@@ -6,18 +6,18 @@ public class EvenNumber {
 	static int inputNumber; 
 	static String result;
 	public static void main(String[] args) {
-		System.out.println("PLease Enter the Number to validate : ");
+		System.out.println("PLease Enter the Number to validate even : ");
 		displayResult(result = validateEven(inputNumber= scan.nextInt()));
 	}
 	public static String validateEven(int inputNumber) {
-		result = "odd";
+		result = "ODD";
 		if (inputNumber % 2 == 0) {
-			result = "even";
+			result = "EVEN";
 		}
 		return result;			
 		}
 	public static void displayResult(String result) {
-		System.out.println("The Give Number is : "+ result);
+		System.out.println("The Give Number is : "+ result + " number");
 	}
 	}
 
--- src/com/orange/practice/OddNumber.java
+++ src/com/orange/practice/OddNumber.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 public class OddNumber {
 	static Scanner scan = new Scanner(System.in);
 	static int inputNumber;
-	static String result = "even";
+	static String result = "EVEN";
 	public static void main(String[] args) {
-		System.out.println("Please Enter the number to : ");
+		System.out.println("Please Enter the number to check ODD : ");
 		displayResult (result = validateOdd(inputNumber = scan.nextInt()));
 	}
 	public static String validateOdd(int inputNumber) {
 		if (inputNumber % 2 !=0 ) {
-			result = "odd";
+			result = "ODD";
 		}
 		return result;
 	}
 	public static void displayResult(String result) {
-		System.out.println("The input Number is : " + result );
+		System.out.println("The input Number is : " + result + " number");
 	}
 }
