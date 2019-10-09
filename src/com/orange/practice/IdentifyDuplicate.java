2d54218ae1303bf27129c33df852f0a8f9c374f4
Durai Devi <duraidevi@Durais-Mac-mini.local>
2019-10-08 23:13:42 -0700

    adding a new class

---
+++ src/com/orange/practice/IdentifyDuplicate.java
@@ -0,0 +1,9 @@
+package com.orange.practice;
+
+public class IdentifyDuplicate {
+    public static void main(String[] args){
+        int[] myint = {1,3,4,5,2,1};
+        System.out.println(myint);
+    }
+
+}
